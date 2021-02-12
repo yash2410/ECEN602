8370a517bd5ba322bf6e975af72c8027c86b8355
Yash J. Doshi <yashdoshi2410@gmail.com>
2021-02-12 16:26:55 -0600

    Add files via upload

--- 2/client.c
+++ 2/client.c
@@ -1,65 +1,83 @@
-#include "common.h"
+#include"common.h"
 
-/*
-Client for using SBCP Chat Protocol
-to run : client  <server_ip> <port> 
+int main(int argc, char *argv[]){
 
-client connects to the server using msg type join and authenticating with the usernames.
-The socket listens for events to recieve from the server(FWD messages from other clients)
-and send messages to the server.
-*/
+    struct sockaddr_in addr_server;
+    int cfd;
+    fd_set read_fd;
+    char username[16];
 
-int main(int argc, char *argv[])
-{
+    if (argc != 3){
+        perror("Usage: echo <IPAddress> <Port>\n");
+        exit(EXIT_FAILURE);
+    }
 
-  struct sockaddr_in addr_server;
-  int cfd;
-  fd_set read_fd;
-  int fd_max;
-  char username[16];
+    memset(username, '\0', sizeof(username));
+    printf("Enter username :\n");
+    fgets(username,16,stdin); //fgets to limit id to 16
 
-  if (argc != 3)
-  {
-    perror("Usage: echo <IPAddress> <Port> \n");
-    exit(EXIT_FAILURE);
-  }
+    char* ip_addr = argv[1];
+    int port = atoi(argv[2]);
 
-  memset(username, 0, sizeof(username));
-  printf("Enter username : \n");
-  fgets(username, 16, stdin);
+    /* ---- Set up client ------ */
+    // Initialize socket using TCP
+    if ((cfd = socket(AF_UNSPEC, SOCK_STREAM, 0)) < 0){ //IP agnostic
+        perror("client socket() error");
+        exit(EXIT_FAILURE);
+	};
+    printf("[+] SOCKET CREATED \n");
 
-  char *ip_addr = argv[1];
-  int port = atoi(argv[2]);
+    addr_server.sin_family = AF_UNSPEC;
+    addr_server.sin_port = htons(port);
+  
+    // Convert from ASCII string to binary network address
+    //AF_INET here because tamu ece servers block IPv6
+    if (inet_pton(AF_INET, ip_addr, &addr_server.sin_addr) <= 0){  
+        perror("client inet_pton() error"); 
+        exit(EXIT_FAILURE);
+    } 
+  
+    // Connect to the server
+    if (connect(cfd, (struct sockaddr *)&addr_server, sizeof(addr_server)) < 0){
+        perror("client connect() error");
+        exit(EXIT_FAILURE);
+    } 
+    printf("[+] CONNECTED TO SERVER \n");
 
-  if ((cfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-  {
-    perror("client socket() error");
-    exit(EXIT_FAILURE);
-  };
-  printf("[+] SOCKET CREATED \n");
+    // TODO : Join server with username authentication
 
-  addr_server.sin_family = AF_INET;
-  addr_server.sin_port = htons(port);
+    printf("[+] USER %s AUTHENTICATED",username);
+    
+    FD_ZERO(&read_fd);
+    FD_SET(0,&read_fd);
+    FD_SET(cfd,&read_fd);
 
-  // Convert from ASCII string to binary network address
-  if (inet_pton(AF_INET, ip_addr, &addr_server.sin_addr) <= 0)
-  {
-    perror("client inet_pton() error");
-    exit(EXIT_FAILURE);
-  }
 
-  // Connect to the server
-  if (connect(cfd, (struct sockaddr *)&addr_server, sizeof(addr_server)) < 0)
-  {
-    perror("client connect() error");
-    exit(EXIT_FAILURE);
-  }
-  printf("[+] CONNECTED TO SERVER \n");
+    /*
+    iterarte through all fd in fd_set and check for any event in an infinite loop.
+    0 --> send to server
+    cfd --> revcieve from server  
+    and ignores anyother fd because it is a client and doesnt have to connect to anyone else
+    */
+    while(1){
+      
+      if(select(cfd+1,&read_fd,NULL,NULL,NULL)<0){
+        perror("client select() error");
+        exit(EXIT_FAILURE);
+      }
+      else{
+        
+        if(FD_ISSET(cfd,&read_fd)){
+          printf("recieve stuff");
+        } 
 
-  //TODO : autenticate with server by sending username
+        if(FD_ISSET(0,&read_fd)){
+          printf("send stuff");
+        }
 
-  FD_ZERO(&read_fd);
-  FD_SET(cfd,&read_fd);
-  FD_SET(0,&read_fd);
+      }
 
+    }
+    
+    return 0;
 }
--- 2/common.h
+++ 2/common.h
@@ -1,35 +1,35 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>		// defines perror(), herror()
-#include <fcntl.h>		// set socket to non-blocking with fcntrl()
+#include <errno.h>		
+#include <fcntl.h>		
 #include <unistd.h>
 #include <string.h>
-#include <assert.h>		//add diagnostics to a program
-#include <netinet/in.h>		//defines in_addr and sockaddr_in structures
-#include <arpa/inet.h>		//external definitions for inet functions
-#include <netdb.h>		//getaddrinfo() & gethostbyname()
-#include <sys/socket.h>		//
+#include <assert.h>		
+#include <netinet/in.h>		
+#include <arpa/inet.h>		
+#include <netdb.h>		
+#include <sys/socket.h>	
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <sys/wait.h>
-#include <sys/select.h>		// for select() system call only
+#include <sys/select.h>	
 #include <sys/time.h>
 
 //msg type
-const struct sbcp_msg_type{
-  const int JOIN = 2;
-  const int FWD = 3;
-  const int SEND = 4;
-};
+#define JOIN 2
+#define FWD 3
+#define SEND 4
 
-//msg attribute types
-const struct sbcp_attribute_type{
-  const int REASON = 1;
-  const int USERNAME = 2;
-  const int CLIENT_COUNT = 3;
-  const int MSG = 4;
-};
+//attribute type
+#define REASON 1
+#define USERNAME 2
+#define CLIENT_COUNT 3
+#define MSG 4
 
 struct sbcp_attribute{
   int type:16;  //set to 16bits (2 byte)
@@ -43,3 +43,32 @@ struct sbcp_message{
   int length:16;  
   struct sbcp_attribute msg_payload;
 };
+
+int writen(int socketfd, char* buffer){
+    int err = send(socketfd, buffer, strlen(buffer), 0);
+    if (err < 0){
+        perror("send() error");
+        exit(EXIT_FAILURE);
+    }
+    printf("[+] MSG SENT: %s\n", buffer);
+
+	return err;
+}
+
+/**
+ * Will read a line of characters from a socket and return the number of bytes
+ * read or a -1 on error.  
+ **/
+int readline(int socketfd, char* buffer, int numBytes) {
+    int err = recv(socketfd, buffer, sizeof(buffer), 0);
+    if (err < 0){
+        perror("recv() error");
+        exit(EXIT_FAILURE);
+    }
+    printf("[+] MSG RECIEVED: %s\n", buffer);
+    
+    return err;
+}
+
+
+#endif
